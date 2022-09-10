12a9263e901e440fbab4d143ca5f981cac2f5e1a
vins7 <kadexyogi7@gmail.com>
2022-09-10 22:55:05 +0700

    first init

---
+++ entity/database.go
@@ -0,0 +1,10 @@
+package entity
+
+// Database :
+type Database struct {
+	Host     string `yaml:"host"`
+	Port     string `yaml:"port"`
+	Username string `yaml:"username"`
+	Password string `yaml:"password"`
+	Dbname   string `yaml:"dbname"`
+}
